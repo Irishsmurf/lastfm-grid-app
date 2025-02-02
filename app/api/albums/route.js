70582cfdbae62c1a495c6ef2851ed86532b38228
Paddez <dave@paddez.com>
2025-02-02 11:17:12 +0000

    Query

--- app/api/albums/route.js
+++ app/api/albums/route.js
@@ -3,6 +3,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(req) {
-    const { username, period } = req.query;
-    return NextResponse.json(`{username: ${username}, period: ${period}})`, { status: 200 });
+    //const { username, period } = req.query;
+    return NextResponse.json(req.query, { status: 200 });
 }
