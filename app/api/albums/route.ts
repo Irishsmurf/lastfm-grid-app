48cca90940e05dc7adbe68e7d884c0137e916440
Dave K <dave@paddez.com>
2025-03-02 20:25:30 +0000

    Testing

--- app/api/albums/route.ts
+++ app/api/albums/route.ts
@@ -1,43 +1,47 @@
 // app/api/albums/route.js
 
-import { NextRequest, NextResponse } from 'next/server';
-import { redis } from '../../../lib/redis';
-
+import { NextRequest, NextResponse } from "next/server";
+import { redis } from "../../../lib/redis";
 
 export async function GET(req: NextRequest) {
-    const { searchParams } = new URL(req.url);
-    const username = searchParams.get('username');
-    const period = searchParams.get('period');
+  const { searchParams } = new URL(req.url);
+  const username = searchParams.get("username");
+  const period = searchParams.get("period");
 
-    const cacheKey = `lastfm:${username}:${period}`;
+  const cacheKey = `lastfm:${username}:${period}`;
 
-    try {
-        // Check cache first
-        const cachedData = await redis.get(cacheKey);
-        if (cachedData) {
-            console.log(`Returning cached data for ${cacheKey}`)
-            return NextResponse.json(JSON.parse(cachedData), { status: 200 });
-        }
-
-        // If not in cache, fetch from LastFM
-        console.log(`Fetching data for ${username} - ${period} from LastFM`)
-        const response = await fetch(
-            `${process.env.LASTFM_BASE_URL}?method=user.gettopalbums&user=${username}&period=${period}&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=9`
-        );
-
-        if (!response.ok) {
-            console.error(`Error response from LastFM: ${response.status}:${response.statusText}`)
-            throw new Error('Failed to fetch from LastFM');
-        }
-
-        const data = await response.json();
-
-        // Cache the response for 1 hour
-        await redis.setex(cacheKey, 3600, JSON.stringify(data));
-
-        return NextResponse.json(data, { status: 200 });
-    } catch (error: any) {
-        console.log(`Error fetching albums: ${error.message}`)
-        return NextResponse.json({ message: 'Error fetching albums', error: error }, { status: 500 });
+  try {
+    // Check cache first
+    const cachedData = await redis.get(cacheKey);
+    if (cachedData) {
+      console.log(`Returning cached data for ${cacheKey}`);
+      return NextResponse.json(JSON.parse(cachedData), { status: 200 });
     }
+
+    // If not in cache, fetch from LastFM
+    console.log(`Fetching data for ${username} - ${period} from LastFM`);
+    const response = await fetch(
+      `${process.env.LASTFM_BASE_URL}?method=user.gettopalbums&user=${username}&period=${period}&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=9`
+    );
+
+    if (!response.ok) {
+      console.error(
+        `Error response from LastFM: ${response.status}:${response.statusText}`
+      );
+      throw new Error("Failed to fetch from LastFM");
+    }
+
+    const data = await response.json();
+
+    // Cache the response for 1 hour
+    await redis.setex(cacheKey, 3600, JSON.stringify(data));
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.log(`Error fetching albums: ${error.message}`);
+    return NextResponse.json(
+      { message: "Error fetching albums", error: error },
+      { status: 500 }
+    );
+  }
 }
