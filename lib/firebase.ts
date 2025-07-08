56c15e8534fb2994ea719e00f73fc44f770b9987
David K <dave@paddez.com>
2025-07-09 00:49:39 +0200

    Dev (#45)
    * Export Defaults
    
    * Export Defaults

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Set default values (optional, but recommended)
-remoteConfig.defaultConfig = {
+export const defaultRemoteConfig = {
   show_footer_feature_text: false, // Default value for our example feature
   shared_grid_expiry_days: 30, // Default expiry in days
   default_time_period: '1month', // Default time period
@@ -50,7 +50,7 @@ remoteConfig.defaultConfig = {
   not_found_cache_expiry_seconds: 86400, // Default not found cache expiry in seconds
   spotify_cache_expiry_seconds: 3600, // Default Spotify cache expiry in seconds
 };
-
+remoteConfig.defaultConfig = defaultRemoteConfig;
 // Call this function when your app starts to fetch and activate the latest config
 export const initializeRemoteConfig = async () => {
   try {
