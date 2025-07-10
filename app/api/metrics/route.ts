b33be660f6802f0ed3a1b197cc0ce5ff78824a47
David Kernan <dave@paddez.com>
2025-07-10 13:49:13 +0100

    Logging

--- app/api/metrics/route.ts
+++ app/api/metrics/route.ts
@@ -1,8 +1,11 @@
 // app/api/metrics/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { registry } from '../../../lib/metrics';
+import { logger } from '@/utils/logger';
+const CTX = 'MetricsAPI';
 
 export async function GET(_req: NextRequest) {
+  logger.info(CTX, 'Received request for metrics');
   const metrics = await registry.metrics();
   return new NextResponse(metrics, {
     status: 200,
